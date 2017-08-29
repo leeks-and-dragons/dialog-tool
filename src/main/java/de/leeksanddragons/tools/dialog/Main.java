79ddfb2d612a1dbff9e4c49aba50ea4d74b79bba
JuKu <kuenzel.justin@t-online.de>
2017-08-29 14:36:18 +0200

    added main class.

---
+++ src/main/java/de/leeksanddragons/tools/dialog/Main.java
@@ -0,0 +1,12 @@
+package de.leeksanddragons.tools.dialog;
+
+/**
+ * Created by Justin on 29.08.2017.
+ */
+public class Main {
+
+    public static void main (String[] args) {
+        //
+    }
+
+}

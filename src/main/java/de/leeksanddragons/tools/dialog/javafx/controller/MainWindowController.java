ecfb23133b4819434adc88eb5a8ab6b653c31b04
JuKu <kuenzel.justin@t-online.de>
2017-08-30 16:49:36 +0200

    added exception handling for save file dialog.

--- src/main/java/de/leeksanddragons/tools/dialog/javafx/controller/MainWindowController.java
+++ src/main/java/de/leeksanddragons/tools/dialog/javafx/controller/MainWindowController.java
@@ -2,8 +2,12 @@ package de.leeksanddragons.tools.dialog.javafx.controller;
 
 import de.leeksanddragons.tools.dialog.i18n.LangLoader;
 import de.leeksanddragons.tools.dialog.javafx.FXMLController;
+import de.leeksanddragons.tools.dialog.javafx.window.OpenFileDialog;
+import de.leeksanddragons.tools.dialog.javafx.window.SaveFileDialog;
 import de.leeksanddragons.tools.dialog.model.LangEntry;
 import de.leeksanddragons.tools.dialog.model.QuestionEntry;
+import de.leeksanddragons.tools.dialog.utils.FileUtils;
+import de.leeksanddragons.tools.dialog.utils.JavaFXUtils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -17,8 +21,12 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.json.JSONObject;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 /**
@@ -73,6 +81,8 @@ public class MainWindowController implements FXMLController, Initializable {
     @FXML
     protected MenuItem closeDialogMenuItem;
 
+    protected String lastSavePath = "";
+
     public MainWindowController (LangLoader loader) {
         this.langLoader = loader;
     }
@@ -121,6 +131,20 @@ public class MainWindowController implements FXMLController, Initializable {
             }
         });
 
+        this.saveDialogMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                openSaveDialog();
+            }
+        });
+
+        this.savePathDialogMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                openSaveToDialog();
+            }
+        });
+
         this.questionList.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
@@ -154,6 +178,9 @@ public class MainWindowController implements FXMLController, Initializable {
         //clear map
         this.questionMap.clear();
 
+        //clear last save path
+        this.lastSavePath = "";
+
         //refresh list
         this.refreshListView();
 
@@ -162,6 +189,59 @@ public class MainWindowController implements FXMLController, Initializable {
 
         //show all widgets
         this.showAllWidgets();
+    }
+
+    protected void openSaveDialog () {
+        if (this.lastSavePath.isEmpty()) {
+            //no save path is given, so open "save to" dialog
+            this.openSaveToDialog();
+
+            return;
+        }
+
+        this.save(this.lastSavePath);
+    }
+
+    protected void openSaveToDialog () {
+        SaveFileDialog dialog = new SaveFileDialog(this.stage);
+        File saveFile = dialog.open();
+
+        //set last save path
+        this.lastSavePath = saveFile.getAbsolutePath();
+
+        //save dialog
+        this.save(this.lastSavePath);
+    }
+
+    protected void openDialog () {
+        OpenFileDialog dialog = new OpenFileDialog(this.stage);
+        File file = dialog.open();
+
+        //set last save path
+        this.lastSavePath = file.getAbsolutePath();
+
+        //open dialog
+        this.load(this.lastSavePath);
+    }
+
+    protected void save (String filePath) {
+        System.out.println("save path: " + filePath);
+
+        //create new json object
+        JSONObject json = new JSONObject();
+
+        //save file
+        try {
+            FileUtils.writeFile(filePath, json.toString(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            JavaFXUtils.showExceptionDialog("Exception", "Exception while saving file", "An exception occurred while save file! Please report to developers! ", e);
+        }
+    }
+
+    protected void load (String filePath) {
+        //
     }
 
     @Override

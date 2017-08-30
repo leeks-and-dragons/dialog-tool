e7c6047ec6f2df3f57c4b7b6bc01141771f1910d
JuKu <kuenzel.justin@t-online.de>
2017-08-30 16:49:24 +0200

    added OpenFileDialog for issue #2 .

---
+++ src/main/java/de/leeksanddragons/tools/dialog/javafx/window/OpenFileDialog.java
@@ -0,0 +1,39 @@
+package de.leeksanddragons.tools.dialog.javafx.window;
+
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+/**
+ * Created by Justin on 30.08.2017.
+ */
+public class OpenFileDialog {
+
+    protected Stage stage = null;
+    protected FileChooser fileChooser = new FileChooser();
+
+    public OpenFileDialog() {
+        //create an new stage
+        this.stage = new Stage();
+
+        this.stage.setTitle("Open...");
+
+        this.stage.hide();
+    }
+
+    public OpenFileDialog(Stage stage) {
+        //create an new stage
+        this.stage = stage;
+    }
+
+    public File open () {
+        //this.stage.setTitle("Save to...");
+
+        this.stage.show();
+
+        fileChooser.setTitle("Open...");
+        return fileChooser.showOpenDialog(stage);
+    }
+
+}

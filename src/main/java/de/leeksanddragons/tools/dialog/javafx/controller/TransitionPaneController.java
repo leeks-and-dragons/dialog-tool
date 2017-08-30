af80d6c73a57484a533557ab28c8d47ae406e8d1
JuKu <kuenzel.justin@t-online.de>
2017-08-31 00:58:15 +0200

    fixed some problems.

--- src/main/java/de/leeksanddragons/tools/dialog/javafx/controller/TransitionPaneController.java
+++ src/main/java/de/leeksanddragons/tools/dialog/javafx/controller/TransitionPaneController.java
@@ -2,11 +2,17 @@ package de.leeksanddragons.tools.dialog.javafx.controller;
 
 import de.leeksanddragons.tools.dialog.javafx.FXMLController;
 import de.leeksanddragons.tools.dialog.model.QuestionEntry;
+import de.leeksanddragons.tools.dialog.model.transition.Transition;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -22,6 +28,9 @@ public class TransitionPaneController implements FXMLController, Initializable {
     protected Button addTransitionButton;
 
     @FXML
+    protected ChoiceBox<String> typeChoiceBox;
+
+    @FXML
     protected ListView<Pane> listView;
 
     public TransitionPaneController (QuestionEntry entry, int index) {
@@ -30,7 +39,23 @@ public class TransitionPaneController implements FXMLController, Initializable {
 
     @Override
     public void init(Stage stage, Scene scene, Pane pane) {
-        //
+        //create list with all transition types
+        ObservableList<String> list = FXCollections.observableArrayList();
+
+        for (String typeName : Transition.listTransitionTypeNames()) {
+            list.add(typeName);
+        }
+
+        //set transition types to choice box
+        typeChoiceBox.setItems(list);
+        typeChoiceBox.getSelectionModel().select(0);
+
+        addTransitionButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                //
+            }
+        });
     }
 
     @Override
